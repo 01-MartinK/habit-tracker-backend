320d2c399fd67722db2b1ba7c6ce07cc37524b43
Martin K <nitram1509@gmail.com>
2025-07-23 13:59:33 +0300

    Refactor imports

--- src/main/kotlin/org/mk/hbtracker/domain/Habit.kt
+++ src/main/kotlin/org/mk/hbtracker/domain/Habit.kt
@@ -1,22 +1,17 @@
 package org.mk.hbtracker.domain
 
-import jakarta.persistence.Entity
-import jakarta.persistence.GeneratedValue
-import jakarta.persistence.Id
-import jakarta.persistence.GenerationType
-import jakarta.persistence.Table
-import java.time.LocalDateTime
+import jakarta.persistence.*
 
 @Entity
 @Table(name = "habits")
 data class Habit(
- @Id
- @GeneratedValue(strategy = GenerationType.IDENTITY)
- var id: Long? = null,
- var name: String,
- var startTime: Int,
- var duration: Int,
- var createdAt: String,
- var modifiedAt: String,
- var userId: String? = null,
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long? = null,
+    var name: String,
+    var startTime: Int,
+    var duration: Int,
+    var createdAt: String,
+    var modifiedAt: String,
+    var userId: String? = null,
 )
--- src/main/kotlin/org/mk/hbtracker/engine/HabitController.kt
+++ src/main/kotlin/org/mk/hbtracker/engine/HabitController.kt
@@ -1,17 +1,10 @@
 package org.mk.hbtracker.engine
 
 import org.mk.hbtracker.domain.CreateHabitRequest
-import org.springframework.web.bind.annotation.RestController
-import org.mk.hbtracker.domain.Habit;
+import org.mk.hbtracker.domain.Habit
 import org.mk.hbtracker.domain.UpdateHabitRequest
 import org.mk.hbtracker.services.HabitService
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.*
 import java.time.LocalDateTime
 
 @RestController
@@ -21,23 +14,27 @@ public class HabitController(private val service: HabitService) {
     fun getHabits(): List<Habit> = service.getAllHabits()
 
     @PostMapping("/")
-    fun createHabit(@RequestBody body: CreateHabitRequest): Habit = service.createHabit(Habit(
-        name = body.name,
-        startTime = body.startTime,
-        duration = body.duration,
-        createdAt = LocalDateTime.now().toString(),
-        modifiedAt = LocalDateTime.now().toString(),
-    ))
+    fun createHabit(@RequestBody body: CreateHabitRequest): Habit = service.createHabit(
+        Habit(
+            name = body.name,
+            startTime = body.startTime,
+            duration = body.duration,
+            createdAt = LocalDateTime.now().toString(),
+            modifiedAt = LocalDateTime.now().toString(),
+        )
+    )
 
     @PutMapping("/{id}")
-    fun updateHabit(@PathVariable id: Long, @RequestBody body: UpdateHabitRequest): Habit = service.updateHabit(id, Habit(
-        id = id,
-        name = body.name,
-        startTime = body.startTime,
-        duration = body.duration,
-        createdAt = LocalDateTime.now().toString(),
-        modifiedAt = LocalDateTime.now().toString()
-    ))
+    fun updateHabit(@PathVariable id: Long, @RequestBody body: UpdateHabitRequest): Habit = service.updateHabit(
+        id, Habit(
+            id = id,
+            name = body.name,
+            startTime = body.startTime,
+            duration = body.duration,
+            createdAt = LocalDateTime.now().toString(),
+            modifiedAt = LocalDateTime.now().toString()
+        )
+    )
 
     @DeleteMapping("/{id}")
     fun deleteHabit(@PathVariable id: Long) = service.deleteHabit(id)
