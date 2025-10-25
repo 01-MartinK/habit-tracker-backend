1fecddc30ab26663e15efff0f881947c823faebe
Martin K <nitram1509@gmail.com>
2025-10-25 21:02:01 +0300

    custom UUID Serializer

--- src/main/kotlin/domain/Habit.kt
+++ src/main/kotlin/domain/Habit.kt
@@ -1,10 +1,12 @@
 package com.habit.domain
 
+import com.habit.util.UUIDSerializer
 import kotlinx.serialization.Serializable
-import java.util.UUID
+import java.util.*
 
 @Serializable
 data class Habit(
+    @Serializable(with = UUIDSerializer::class)
     val id: UUID,
     val icon: String,
     val title: String,
